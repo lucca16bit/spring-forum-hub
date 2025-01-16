ade0363422d32c748f58fa1d2c6db8af84010a99
lucas <lucaston2323@gmail.com>
2025-01-16 13:18:37 -0300

    fix: refazendo migrations

--- src/main/resources/db/migration/V2__create-table-topico.sql
+++ src/main/resources/db/migration/V2__create-table-topico.sql
@@ -4,10 +4,10 @@ create table topico (
     mensagem text not null,
     data_criacao timestamp not null,
     status varchar(255) not null default 'NAO_RESPONDIDO',
-    autor bigint not null,
+    autor_id bigint not null,
     curso varchar(100) not null,
     respostas bigint,
 
     primary key(id),
-    foreign key(autor) references usuario(id)
+    foreign key(autor_id) references usuario(id)
 );
