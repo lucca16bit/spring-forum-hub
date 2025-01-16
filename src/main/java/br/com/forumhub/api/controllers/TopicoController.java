6d8e244dea2e9a10b49e532974533efb18f01283
lucas <lucaston2323@gmail.com>
2025-01-16 15:10:17 -0300

    feat: endpoint para detalhar algum topico cadastrado

--- src/main/java/br/com/forumhub/api/controllers/TopicoController.java
+++ src/main/java/br/com/forumhub/api/controllers/TopicoController.java
@@ -13,9 +13,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
@@ -49,5 +51,13 @@ public class TopicoController {
                 .map(ListagemTopicosDto::new);
 
         return ResponseEntity.ok(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalharTopico(@PathVariable Long id) {
+        var topico = repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Id do tópico não existe"));
+
+        return ResponseEntity.ok(new DetalhesTopicoDto(topico));
     }
 }
